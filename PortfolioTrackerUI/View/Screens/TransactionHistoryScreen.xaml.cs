458be6c73ac7db47822bf72126eab04172787e43
Lior <lior-katz@campus.technion.ac.il>
2023-11-12 16:56:29 +0200

    Created initial UI

---
+++ PortfolioTrackerUI/View/Screens/TransactionHistoryScreen.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PortfolioTrackerUI.View.Screens
+{
+	/// <summary>
+	/// Interaction logic for TransactionHistoryScreen.xaml
+	/// </summary>
+	public partial class TransactionHistoryScreen : UserControl
+	{
+		public TransactionHistoryScreen()
+		{
+			InitializeComponent();
+		}
+	}
+}
