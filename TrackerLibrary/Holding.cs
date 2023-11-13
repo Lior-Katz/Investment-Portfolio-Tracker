a4c897a4abcaba5219a121f9aa9e1e39dbdbced6
Lior <lior-katz@campus.technion.ac.il>
2023-11-14 01:17:01 +0200

    Changed constructor and added functions to add and remove trades from list, implemented operator == and != as well as .Equals

--- TrackerLibrary/Holding.cs
+++ TrackerLibrary/Holding.cs
@@ -1,11 +1,11 @@
 ﻿namespace TrackerLibrary
 {
-	public class Holding
+	public class Holding : IEquatable<Holding>
 	{
 		/// <summary>
 		/// Unique identifier
 		/// </summary>
-		public int Id { get; set; }
+		public int Id { get; private set; }
 		/// <summary>
 		/// Asset Name
 		/// </summary>
@@ -25,7 +25,7 @@
 		/// <summary>
 		/// Initial date when this asset was aquired.
 		/// </summary>
-		public DateTime AquisitionDate { get; set; }
+		public DateOnly AquisitionDate { get; set; }
 		/// <summary>
 		/// The periodical payout of this asset.
 		/// </summary>
@@ -47,7 +47,7 @@
 		public string Market { get; set; }
 
 		/// <summary>
-		/// Constructor
+		/// Initializes a new instance of the Holding class that has an empty Trades list.
 		/// </summary>
 		/// <param name="name">Asset name </param>
 		/// <param name="ticker">Ticker symbol</param>
@@ -57,17 +57,58 @@
 		/// <param name="type">Type of investment vehicle</param>
 		/// <param name="secotr">Sector</param>
 		/// <param name="market">Market</param>
-		public Holding(string name, string ticker, decimal quantity, DateTime aquisitionDate, Payout payout, string type, string secotr, string market)
+		public Holding(string name, string ticker, decimal quantity, DateOnly aquisitionDate, decimal yield, decimal payoutTax, decimal payoutCommission, int payoutPeriodInMonths, string type, string secotr, string market)
 		{
 			this.Name = name;
 			this.Ticker = ticker;
 			this.Quantity = quantity;
 			this.AquisitionDate = aquisitionDate;
-			this.Payout = payout;
+			this.Payout = new Payout(yield, payoutTax, payoutCommission, payoutPeriodInMonths, aquisitionDate);
 			this.Type = type;
 			this.Sector = secotr;
 			this.Market = market;
+		}
 
+		public void addTrade(Trade trade)
+		{
+			if (trade == null)
+				throw new NullReferenceException("null trade added");
+
+			if (Trades.Contains(trade))
+				return;
+
+			Trades.Add(trade);
+		}
+
+		public void removeTrade(Trade trade)
+		{
+			Trades.Remove(trade);
+		}
+
+		public override bool Equals(object? other)
+		{
+			return other is Holding holding && this.Id == holding.Id;
+		}
+
+		public bool Equals(Holding? other)
+		{
+			return other is not null && this.Id == other.Id;
+		}
+
+		public static bool operator ==(Holding? lvalue, Holding? rvalue)
+		{
+			return (lvalue is null && rvalue is null) ||
+				(lvalue is not null && lvalue.Equals(rvalue));
+		}
+
+		public static bool operator !=(Holding? lvalue, Holding? rvalue)
+		{
+			return !(lvalue == rvalue);
+		}
+
+		public override int GetHashCode()
+		{
+			return Id.GetHashCode();
 		}
 	}
 }
