a270454cf8307336e965d8d3a5a869d7142a8f98
Lior <lior-katz@campus.technion.ac.il>
2023-11-14 01:19:04 +0200

    Implemented constructor, == and != operators as well as .Equals()

--- TrackerLibrary/Trade.cs
+++ TrackerLibrary/Trade.cs
@@ -1,19 +1,24 @@
 ﻿namespace TrackerLibrary
 {
-	public class Trade
+	public class Trade : IEquatable<Trade>
 	{
 		/// <summary>
 		/// Unique identifier.
 		/// </summary>
 		public int Id { get; set; }
+
 		/// <summary>
 		/// The name of the asset traded.
 		/// </summary>
 		public string Name { get; set; } = string.Empty;
+
 		/// <summary>
 		/// The ticker symbol of the asset traded.
 		/// </summary>
 		public string Ticker { get; set; } = string.Empty;
+
+		bool IsBuyOrder { get; set; }
+
 		/// <summary>
 		/// The date when the trade took place.
 		/// </summary>
@@ -34,5 +39,52 @@
 		/// Total commission paid on the trade
 		/// </summary>
 		public decimal Commission { get; set; }
+
+		public Trade(string name, string ticker, bool isBuyOrder, DateTime date, string quantity, string price, string tax, string commission)
+		{
+			this.Name = name;
+			this.Ticker = ticker;
+			this.IsBuyOrder = isBuyOrder;
+			this.Date = date;
+
+			decimal quantityValue, priceValue, taxValue, commissionValue;
+			decimal.TryParse(quantity, out quantityValue);
+			decimal.TryParse(price, out priceValue);
+			decimal.TryParse(tax, out taxValue);
+			decimal.TryParse(commission, out commissionValue);
+
+			this.Quantity = quantityValue;
+			this.Price = priceValue;
+			this.Tax = taxValue;
+			this.Commission = commissionValue;
+		}
+
+		public static bool operator ==(Trade? lvalue, Trade? rvalue)
+		{
+			if (lvalue is null && rvalue is null)
+				return true;
+
+			return lvalue is not null && lvalue.Equals(rvalue);
+		}
+
+		public static bool operator !=(Trade? lvalue, Trade? rvalue)
+		{
+			return !(lvalue == rvalue);
+		}
+
+		public bool Equals(Trade? obj)
+		{
+			return obj is Trade trade && this.Id == trade.Id;
+		}
+
+		public override bool Equals(object? obj)
+		{
+			return obj is Trade trade && this.Id == trade.Id;
+		}
+
+		public override int GetHashCode()
+		{
+			return this.Id.GetHashCode();
+		}
 	}
 }
