20ba4b1e9e21c6dc42a13955d69590dab0b29df9
Lior <lior-katz@campus.technion.ac.il>
2023-11-14 01:18:02 +0200

    Added functions to remove and add trades and holdings

--- TrackerLibrary/Portfolio.cs
+++ TrackerLibrary/Portfolio.cs
@@ -5,7 +5,7 @@
 		/// <summary>
 		/// The unique identifier of the portfolio.
 		/// </summary>
-		public int Id { get; set; }
+		public int Id { get; private set; }
 		/// <summary>
 		/// The name of the portfolio.
 		/// </summary>
@@ -23,6 +23,47 @@
 		/// Constructor
 		/// </summary>
 		/// <param name="name">Portfolio Name</param>
+		public Portfolio(string name)
+		{
+			this.Name = name;
+			Holdings = new List<Holding>();
+			Trades = new List<Trade>();
+		}
+
+		public void addTrade(Trade trade)
+		{
+			if (trade == null)
+				throw new NullReferenceException("null trade added");
+
+			if (Trades.Contains(trade))
+				return;
+
+			Trades.Add(trade);
+		}
+
+		public void removeTrade(Trade trade)
+		{
+			Trades.Remove(trade);
+		}
+
+		public void addHolding(Holding holding)
+		{
+			if (holding == null)
+				throw new NullReferenceException();
+
+			if (Holdings.Contains(holding))
+				return;
+
+			Holdings.Add(holding);
+		}
+
+		public void removeHolding(Holding holding)
+		{
+			Holdings.Remove(holding);
+		}
+
+
+
 
 		// TODO: allow tracking of historical portfolio performance. 
 		//public PortfolioHistory PortfolioHistory { get; set; }
