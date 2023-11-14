2ccd4331ff133a248473df22f38ee737380a08de
Lior <lior-katz@campus.technion.ac.il>
2023-11-14 16:23:18 +0200

    Updated to MVVM model, code taken out of View and put into ViewModel

--- PortfolioTrackerUI/MainWindow.xaml.cs
+++ PortfolioTrackerUI/MainWindow.xaml.cs
@@ -14,65 +14,66 @@ namespace PortfolioTrackerUI
 		{
 			InitializeComponent();
 
-			// Subscribe to SwitchToHoldingsScreenRequested event in MainScreen.
-			MainScreen.SwitchToHoldingsScreenRequested += MainScreen_SwitchToHoldingsScreenRequested;
+			
+			//// Subscribe to SwitchToHoldingsScreenRequested event in MainScreen.
+			//MainScreen.SwitchToHoldingsScreenRequested += MainScreen_SwitchToHoldingsScreenRequested;
 
-			// Subscribe to SwitchToTransactionScreenScreenRequested event in MainScreen.
-			MainScreen.SwitchToTransactionHistoryScreenRequested += MainScreen_SwitchToTransactionHistoryScreenRequested;
+			//// Subscribe to SwitchToTransactionScreenScreenRequested event in MainScreen.
+			//MainScreen.SwitchToTransactionHistoryScreenRequested += MainScreen_SwitchToTransactionHistoryScreenRequested;
 
-			// Subscribe to SwitchToAddTransactionScreenScreenRequested event in MainScreen.
-			MainScreen.SwitchToAddTransactionScreenRequested += MainScreen_SwitchToAddTransactionScreenRequested;
+			//// Subscribe to SwitchToAddTransactionScreenScreenRequested event in MainScreen.
+			//MainScreen.SwitchToAddTransactionScreenRequested += MainScreen_SwitchToAddTransactionScreenRequested;
 
-			// Subscribe to SwitchToDistributionsScreenScreenRequested event in MainScreen.
-			MainScreen.SwitchToDistributionsScreenRequested += MainScreen_SwitchToDistributionsScreenRequested;
+			//// Subscribe to SwitchToDistributionsScreenScreenRequested event in MainScreen.
+			//MainScreen.SwitchToDistributionsScreenRequested += MainScreen_SwitchToDistributionsScreenRequested;
 
-			// Subscribe to SwitchToMainScreenRequested event in Banner
-			Banner.SwitchToMainScreenRequested += Banner_SwitchToDistributionsScreenRequested;
+			//// Subscribe to SwitchToMainScreenRequested event in Banner
+			//Banner.SwitchToMainScreenRequested += Banner_SwitchToDistributionsScreenRequested;
 
-			// Subscribe to SwitchToAddTransactionScreenRequested event in TransactionHistoryScreen
-			TransactionHistoryScreen.SwitchToAddTransactionScreenRequested += TransactionHistoryScreen_SwitchToAddTransactionScreenRequested;
+			//// Subscribe to SwitchToAddTransactionScreenRequested event in TransactionHistoryScreen
+			//TransactionHistoryScreen.SwitchToAddTransactionScreenRequested += TransactionHistoryScreen_SwitchToAddTransactionScreenRequested;
 
-			AddTransactionScreen.SwitchToMainScreenRequested += AddTransactionScreen_SwitchToMainScreenRequested;
+			//AddTransactionScreen.SwitchToMainScreenRequested += AddTransactionScreen_SwitchToMainScreenRequested;
 		}
 
-		public void MainScreen_SwitchToHoldingsScreenRequested(object sender, EventArgs e)
-		{
-			HoldingsScreen.Visibility = Visibility.Visible;
-		}
+		//public void MainScreen_SwitchToHoldingsScreenRequested(object sender, EventArgs e)
+		//{
+		//	HoldingsScreen.Visibility = Visibility.Visible;
+		//}
 
-		public void MainScreen_SwitchToTransactionHistoryScreenRequested(object sender, EventArgs e)
-		{
-			TransactionHistoryScreen.Visibility = Visibility.Visible;
-		}
+		//public void MainScreen_SwitchToTransactionHistoryScreenRequested(object sender, EventArgs e)
+		//{
+		//	TransactionHistoryScreen.Visibility = Visibility.Visible;
+		//}
 
-		public void MainScreen_SwitchToAddTransactionScreenRequested(object sender, EventArgs e)
-		{
-			AddTransactionScreen.Visibility = Visibility.Visible;
-		}
+		//public void MainScreen_SwitchToAddTransactionScreenRequested(object sender, EventArgs e)
+		//{
+		//	AddTransactionScreen.Visibility = Visibility.Visible;
+		//}
 
-		public void MainScreen_SwitchToDistributionsScreenRequested(object sender, EventArgs e)
-		{
-			DistributionsScreen.Visibility = Visibility.Visible;
-		}
+		//public void MainScreen_SwitchToDistributionsScreenRequested(object sender, EventArgs e)
+		//{
+		//	DistributionsScreen.Visibility = Visibility.Visible;
+		//}
 
-		public void Banner_SwitchToDistributionsScreenRequested(object sender, EventArgs e)
-		{
-			HoldingsScreen.Visibility = Visibility.Collapsed;
-			TransactionHistoryScreen.Visibility = Visibility.Collapsed;
-			AddTransactionScreen.Visibility = Visibility.Collapsed;
-			DistributionsScreen.Visibility = Visibility.Collapsed;
+		//public void Banner_SwitchToDistributionsScreenRequested(object sender, EventArgs e)
+		//{
+		//	HoldingsScreen.Visibility = Visibility.Collapsed;
+		//	TransactionHistoryScreen.Visibility = Visibility.Collapsed;
+		//	AddTransactionScreen.Visibility = Visibility.Collapsed;
+		//	DistributionsScreen.Visibility = Visibility.Collapsed;
 
-			MainScreen.Visibility = Visibility.Visible;
-		}
+		//	MainScreen.Visibility = Visibility.Visible;
+		//}
 
-		public void TransactionHistoryScreen_SwitchToAddTransactionScreenRequested(object sender, EventArgs e)
-		{
-			AddTransactionScreen.Visibility = Visibility.Visible;
-		}
+		//public void TransactionHistoryScreen_SwitchToAddTransactionScreenRequested(object sender, EventArgs e)
+		//{
+		//	AddTransactionScreen.Visibility = Visibility.Visible;
+		//}
 
-		public void AddTransactionScreen_SwitchToMainScreenRequested(object sender, EventArgs e)
-		{
-			MainScreen.Visibility = Visibility.Visible;
-		}
+		//public void AddTransactionScreen_SwitchToMainScreenRequested(object sender, EventArgs e)
+		//{
+		//	MainScreen.Visibility = Visibility.Visible;
+		//}
 	}
 }
