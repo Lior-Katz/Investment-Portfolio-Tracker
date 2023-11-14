b6fdaa40acd9aea06375849dd395c2b8f51af7d8
Lior <lior-katz@campus.technion.ac.il>
2023-11-14 16:24:04 +0200

    MainWindow intansiation

--- PortfolioTrackerUI/App.xaml.cs
+++ PortfolioTrackerUI/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using PortfolioTracker.ViewModels;
+using System.Windows;
 
 namespace PortfolioTrackerUI
 {
@@ -7,5 +8,16 @@ namespace PortfolioTrackerUI
 	/// </summary>
 	public partial class App : Application
 	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			MainWindow = new MainWindow()
+			{
+				DataContext = new MainViewModel()
+			};
+
+			MainWindow.Show();
+
+			base.OnStartup(e);
+		}
 	}
 }
