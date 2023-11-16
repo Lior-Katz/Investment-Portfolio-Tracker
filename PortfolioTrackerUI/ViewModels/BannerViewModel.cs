52d8890af5fffa2530dec2da2a9c3f75edbaa8d3
Lior <lior-katz@campus.technion.ac.il>
2023-11-17 00:43:39 +0200

    Created Navigation to Dashboard from banner

---
+++ PortfolioTrackerUI/ViewModels/BannerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using PortfolioTracker.Commands;
+using PortfolioTracker.Stores;
+using System.Windows.Input;
+
+namespace PortfolioTracker.ViewModels
+{
+	public class BannerViewModel : ViewModelBase
+	{
+		public ICommand NavigateToDashboardCommand { get; }
+
+		public BannerViewModel(NavigationStore navigationStore)
+		{
+			this.NavigateToDashboardCommand = new NavigateToDashboardCommand(navigationStore);
+		}
+	}
+}
--- PortfolioTrackerUI/ViewModels/MainViewModel.cs
+++ PortfolioTrackerUI/ViewModels/MainViewModel.cs
@@ -4,12 +4,16 @@ namespace PortfolioTracker.ViewModels
 {
 	class MainViewModel : ViewModelBase
 	{
+		private ViewModelBase _bannerViewModel;
+		public ViewModelBase BannerViewModel => _bannerViewModel;
+
 		private readonly NavigationStore _navigationStore;
 
 		public ViewModelBase CurrentViewModel => _navigationStore.CurrentViewModel;
 		public MainViewModel(NavigationStore navigationStore)
 		{
 			_navigationStore = navigationStore;
+			_bannerViewModel = new BannerViewModel(navigationStore);
 
 			_navigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
 		}
