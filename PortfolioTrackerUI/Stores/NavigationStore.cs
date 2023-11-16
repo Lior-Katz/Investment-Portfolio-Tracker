f6ced84debfbb83a25601e319af9a9d95786af0e
Lior <lior-katz@campus.technion.ac.il>
2023-11-16 22:57:25 +0200

    Created NavigationStore

---
+++ PortfolioTrackerUI/Stores/NavigationStore.cs
@@ -0,0 +1,28 @@
+﻿using PortfolioTracker.ViewModels;
+using System;
+
+namespace PortfolioTracker.Stores;
+public class NavigationStore
+{
+	public event Action CurrentViewModelChanged;
+
+	private ViewModelBase _currentViewModel;
+	public ViewModelBase CurrentViewModel
+	{
+		get => _currentViewModel;
+
+		set
+		{
+			_currentViewModel = value;
+			OnCurrentViewModelChanged();
+		}
+	}
+
+	private void OnCurrentViewModelChanged()
+	{
+		CurrentViewModelChanged?.Invoke();
+	}
+
+	//public PortfolioViewModel _portfolio { get; }
+
+}
