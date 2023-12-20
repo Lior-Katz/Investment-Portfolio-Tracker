8b9a4661470bc3cafb2c7540ca10b1e9affa98f8
Lior <lior-katz@campus.technion.ac.il>
2023-12-20 22:43:38 +0200

    Add fetching of value history

--- PortfolioTracker/Services/FinancialDataService.cs
+++ PortfolioTracker/Services/FinancialDataService.cs
@@ -1,5 +1,6 @@
 ﻿using Fynance;
 using Fynance.Result;
+using System;
 using System.Collections.Generic;
 
 namespace PortfolioTracker.Services
@@ -40,5 +41,41 @@ namespace PortfolioTracker.Services
 				yield return GetDailyChange(ticker);
 			}
 		}
+
+		public static IEnumerable<KeyValuePair<TDate, decimal>> GetHistoricalValue<TDate>(string ticker, DateTime startDate)
+		{
+			FyResult result = Ticker.Build(ticker)
+			.SetStartDate(startDate)
+			.SetInterval(Interval.OneDay)
+			.Get();
+
+			foreach (var quote in result.Quotes)
+			{
+				if (typeof(TDate) == typeof(DateTime))
+				{
+					yield return new KeyValuePair<TDate, decimal>((TDate) (object) quote.Period, quote.Close);
+				}
+				else if (typeof(TDate) == typeof(DateOnly))
+				{
+					yield return new KeyValuePair<TDate, decimal>((TDate) (object) DateOnly.FromDateTime(quote.Period), quote.Open);
+				}
+				else
+				{
+					throw new ArgumentException("TDate must be " + typeof(DateOnly) + " or " + typeof(DateTime) + ", cannot be " + typeof(TDate));
+				}
+			}
+		}
+
+		public static IEnumerable<KeyValuePair<TDate, decimal>> GetHistoricalValue<TDate>(string ticker, DateOnly startDate)
+		{
+			return GetHistoricalValue<TDate>(ticker, startDate.ToDateTime(new TimeOnly(0)));
+		}
+
+		public static IEnumerable<KeyValuePair<TDate, decimal>> GetHistoricalValue<TDate>(string ticker, TimeSpan interval)
+		{
+			return GetHistoricalValue<TDate>(ticker, DateTime.Now - interval);
+		}
+
+
 	}
 }
