b79c8de3f61bc01d499cc6b7a8828cfdde92e963
Lior <lior-katz@campus.technion.ac.il>
2024-01-01 19:50:18 +0200

    Add historcial data graph

--- PortfolioTracker/ViewModels/DashboardViewModel.cs
+++ PortfolioTracker/ViewModels/DashboardViewModel.cs
@@ -1,6 +1,12 @@
-﻿using PortfolioTracker.Commands;
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using PortfolioTracker.Commands;
+using PortfolioTracker.Services;
 using PortfolioTracker.Stores;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Input;
 
 namespace PortfolioTracker.ViewModels;
@@ -29,7 +35,75 @@ public class DashboardViewModel : ViewModelBase
 		NavigateToTransactionHistoryCommand = new NavigateCommand<TransactionHistoryViewModel>(navigationStore, () => new TransactionHistoryViewModel(portfolioViewModel, navigationStore));
 		NavigateToDistributionsCommand = new NavigateCommand<DistributionsViewModel>(navigationStore, () => new DistributionsViewModel(portfolioViewModel));
 
-		HistoricValuesLineGraph = new LineGraphViewModel();
+		HistoricValuesLineGraph = GetHistoricalValuesLineGraph(portfolioViewModel,
+			new TimeSpan(365, 0, 0, 0));
+	}
+
+	private LineGraphViewModel GetHistoricalValuesLineGraph(PortfolioViewModel portfolioViewModel, TimeSpan timeSpan)
+	{
+		List<KeyValuePair<DateTime, decimal>> dateValuePairs = GetDateValueList(portfolioViewModel, timeSpan);
+
+		return new LineGraphViewModel(new ISeries[]
+		{
+			new LineSeries<decimal>{
+				Values = dateValuePairs.ConvertAll(x => x.Value)
+			}
+		},
+		new List<Axis>
+		{
+			new Axis{
+				Labels = dateValuePairs.ConvertAll(x => x.Key.ToString("d"))
+			}
+		});
+	}
+
+	private List<KeyValuePair<DateTime, decimal>> GetDateValueList(PortfolioViewModel portfolioViewModel, TimeSpan interval)
+	{
+		TimeSpan graphTimeSpan = new TimeSpan(0);
+		List<List<KeyValuePair<DateTime, decimal>>> AllSecuritiesData = new List<List<KeyValuePair<DateTime, decimal>>>();
+
+		List<string> tickers = portfolioViewModel.Holdings.Select(holding => holding.Ticker).ToList();
+
+		foreach (string ticker in tickers)
+		{
+			DateTime acquisitionDate = portfolioViewModel.Holdings.ToList().Find(holding => holding.Ticker == ticker).AcquisitionDate.ToDateTime(TimeOnly.MinValue);
+
+			TimeSpan timeSpan = TimeSpan.FromTicks(Math.Min(interval.Ticks, (DateTime.Now - acquisitionDate).Ticks));
+			graphTimeSpan = TimeSpan.FromTicks(Math.Max(graphTimeSpan.Ticks, timeSpan.Ticks));
+
+			// create a list of the value of the security on each date
+			List<KeyValuePair<DateTime, decimal>> historicalSecurityValues = FinancialDataService.GetHistoricalValue<DateTime>(ticker, timeSpan).ToList();
+
+			AllSecuritiesData.Add(historicalSecurityValues);
+		}
+
+		List<KeyValuePair<DateTime, decimal>> dateValuePairs = new List<KeyValuePair<DateTime, decimal>>(); // a list of the total value on each date
+		for (int days = graphTimeSpan.Days; days >= 0; --days)
+		{
+			DateTime date = DateTime.Now - new TimeSpan(days, 0, 0, 0);
+
+
+			// a list of the values of every security on a specific date
+			// for every security in AllSecuritiesData, find the pair corresponding to the specific date, and get its value.
+			List<decimal> valuesOnDate = AllSecuritiesData.ConvertAll(securityData => getLastValueBeforeDate(securityData, date));
+
+			// sum over every value on this specific date.
+			dateValuePairs.Add(new KeyValuePair<DateTime, decimal>(date, valuesOnDate.Sum()));
+
+		}
+		return dateValuePairs;
+	}
+
+	private decimal getLastValueBeforeDate(List<KeyValuePair<DateTime, decimal>> securityData, DateTime date)
+	{
+		if (date.Date < securityData.First().Key.Date)
+		{
+			return 0;
+		}
+		KeyValuePair<DateTime, decimal> valueOnDate = securityData.Find(pair => pair.Key.Date == date.Date);
+
+		// if valueOnDate is a default pair, no value is available for this date, so use the value from the day before.
+		return valueOnDate.Equals(default(KeyValuePair<DateTime, decimal>)) ? getLastValueBeforeDate(securityData, date.AddDays(-1)) : valueOnDate.Value;
 	}
 
 	public DashboardViewModel(ObservableCollection<HoldingViewModel> mostInfluential_holdingViewModels, NavigationStore navigationStore, CurrencyModel selectedCurrency)
--- PortfolioTracker/ViewModels/LineGraphViewModel.cs
+++ PortfolioTracker/ViewModels/LineGraphViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.VisualElements;
+using System.Collections.Generic;
 
 namespace PortfolioTracker.ViewModels
 {
@@ -8,11 +10,21 @@ namespace PortfolioTracker.ViewModels
 	{
 		public ISeries[] Series { get; set; }
 
-		public LabelVisual Title { get; set; }
+		public LabelVisual? Title { get; set; }
 
-		public LineGraphViewModel()
+		public List<Axis> XAxes { get; set; }
+
+		public LineGraphViewModel(ISeries[] series, LabelVisual title, List<Axis> xAxes)
 		{
+			this.Series = series;
+			this.Title = title;
+			this.XAxes = xAxes;
+		}
 
+		public LineGraphViewModel(ISeries[] series, List<Axis> xAxes)
+		{
+			this.Series = series;
+			this.XAxes = xAxes;
 		}
 	}
 }
