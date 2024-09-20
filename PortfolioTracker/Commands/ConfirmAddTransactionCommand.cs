e0f714f97961b047e051869bde75eecb78032347
lkatz <lior.katz@intel.com>
2024-09-21 00:13:26 +0300

    formatting

--- PortfolioTracker/Commands/ConfirmAddTransactionCommand.cs
+++ PortfolioTracker/Commands/ConfirmAddTransactionCommand.cs
@@ -9,19 +9,20 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace PortfolioTracker.Commands
 {
-	public class ConfirmAddTransactionCommand : NavigateCommand<TransactionHistoryViewModel>
-	{
-		/// <summary>
-		/// The view model that contains the input from the user through the AddTransactionView
-		/// </summary>
-		private readonly AddTransactionViewModel _addTransactionViewModel;
+    public class ConfirmAddTransactionCommand : NavigateCommand<TransactionHistoryViewModel>
+    {
+        /// <summary>
+        /// The view model that contains the input from the user through the AddTransactionView
+        /// </summary>
+        private readonly AddTransactionViewModel _addTransactionViewModel;
 
-		private readonly AddTransactionDialogViewModel _addTransactionDialogViewModel = new AddTransactionDialogViewModel();
+        private readonly AddTransactionDialogViewModel _addTransactionDialogViewModel =
+            new AddTransactionDialogViewModel();
 
-		/// <summary>
-		/// The portfolio to which the new transaction should be added
-		/// </summary>
-		private readonly PortfolioViewModel _portfolio;
+        /// <summary>
+        /// The portfolio to which the new transaction should be added
+        /// </summary>
+        private readonly PortfolioViewModel _portfolio;
 
 
         /// <summary>
@@ -39,114 +40,116 @@ namespace PortfolioTracker.Commands
             this._addTransactionViewModel = addTransactionViewModel;
             this._portfolio = portfolio;
 
-			// subscribe to onPropertyChanged Event of AddTransactionViewModel for updating canExecute
-			_addTransactionViewModel.PropertyChanged += OnViewModelPropertyChanged;
-		}
+            // subscribe to onPropertyChanged Event of AddTransactionViewModel for updating canExecute
+            _addTransactionViewModel.PropertyChanged += OnViewModelPropertyChanged;
+        }
 
-		/// <summary>
-		/// Checks whether the command should execute in the current state by validating user input.
-		/// </summary>
-		/// <param name="parameter">This parameter is not used, and can be set to null.</param>
-		/// <returns></returns>
-		public override bool CanExecute(object? parameter)
-		{
-			// additional validity checks must be updated in OnViewModelPropertyChanged
-			bool isNameValid = !string.IsNullOrEmpty(_addTransactionViewModel.Name);
-			bool isDateValid = _addTransactionViewModel.Date <= DateTime.Now;
-			bool isQuantityValid = _addTransactionViewModel.Quantity > 0;
-			bool isPriceValid = _addTransactionViewModel.Rate > 0;
+        /// <summary>
+        /// Checks whether the command should execute in the current state by validating user input.
+        /// </summary>
+        /// <param name="parameter">This parameter is not used, and can be set to null.</param>
+        /// <returns></returns>
+        public override bool CanExecute(object? parameter)
+        {
+            // additional validity checks must be updated in OnViewModelPropertyChanged
+            bool isNameValid = !string.IsNullOrEmpty(_addTransactionViewModel.Name);
+            bool isDateValid = _addTransactionViewModel.Date <= DateTime.Now;
+            bool isQuantityValid = _addTransactionViewModel.Quantity > 0;
+            bool isPriceValid = _addTransactionViewModel.Rate > 0;
 
-			return isNameValid && isDateValid && isQuantityValid && isPriceValid && base.CanExecute(parameter);
-		}
+            return isNameValid && isDateValid && isQuantityValid && isPriceValid && base.CanExecute(parameter);
+        }
 
-		/// <summary>
-		/// Executes the command by adding a new transaction that contains all the information from the user input to the portfolio.
-		/// </summary>
-		/// <param name="parameter">This parameter is not used, and can be set to null.</param>
-		public override void Execute(object? parameter)
-		{
-			// Check if there is already a holding with a matching ticker symbol in the portfolio.
-			bool isHoldingExist = _portfolio.isHoldingExist(_addTransactionViewModel.Ticker);
+        /// <summary>
+        /// Executes the command by adding a new transaction that contains all the information from the user input to the portfolio.
+        /// </summary>
+        /// <param name="parameter">This parameter is not used, and can be set to null.</param>
+        public override void Execute(object? parameter)
+        {
+            // Check if there is already a holding with a matching ticker symbol in the portfolio.
+            bool isHoldingExist = _portfolio.isHoldingExist(_addTransactionViewModel.Ticker);
 
-			if (!isHoldingExist && !getAdditionalInfo())
-			{
-				return;
-			}
+            if (!isHoldingExist && !getAdditionalInfo())
+            {
+                return;
+            }
 
-			decimal taxPaid = _addTransactionViewModel.TaxRate * _addTransactionViewModel.Rate * _addTransactionViewModel.Quantity;
-			decimal commissionPaid = _addTransactionViewModel.CommissionRate * _addTransactionViewModel.Rate * _addTransactionViewModel.Quantity;
+            decimal taxPaid = _addTransactionViewModel.TaxRate * _addTransactionViewModel.Rate *
+                              _addTransactionViewModel.Quantity;
+            decimal commissionPaid = _addTransactionViewModel.CommissionRate * _addTransactionViewModel.Rate *
+                                     _addTransactionViewModel.Quantity;
 
-			Trade trade = new Trade(_addTransactionViewModel.Name,
-				_addTransactionViewModel.Ticker,
-				 _addTransactionViewModel.IsBuyOrder,
-				   DateOnly.FromDateTime(_addTransactionViewModel.Date),
-					 _addTransactionViewModel.Quantity.ToString(),
-					 _addTransactionViewModel.Rate.ToString(),
-					 taxPaid.ToString(),
-						commissionPaid.ToString(),
-						   new CurrencyModel(_addTransactionViewModel.Currency));
+            Trade trade = new Trade(_addTransactionViewModel.Name,
+                                    _addTransactionViewModel.Ticker,
+                                    _addTransactionViewModel.IsBuyOrder,
+                                    DateOnly.FromDateTime(_addTransactionViewModel.Date),
+                                    _addTransactionViewModel.Quantity.ToString(),
+                                    _addTransactionViewModel.Rate.ToString(),
+                                    taxPaid.ToString(),
+                                    commissionPaid.ToString(),
+                                    new CurrencyModel(_addTransactionViewModel.Currency));
 
-			trade = _portfolio.AddTransaction(trade);
+            trade = _portfolio.AddTransaction(trade);
 
-			if (isHoldingExist)
-			{
-				base.Execute(parameter);
-				return;
-			}
+            if (isHoldingExist)
+            {
+                base.Execute(parameter);
+                return;
+            }
 
 
-			Holding holding = new Holding(_addTransactionViewModel.Name,
-					_addTransactionViewModel.Ticker,
-					 _addTransactionViewModel.Quantity,
-					 DateOnly.FromDateTime(_addTransactionViewModel.Date),
-					 _addTransactionDialogViewModel.PayoutYield,
-					 _addTransactionDialogViewModel.PayoutTax,
-					 _addTransactionDialogViewModel.PayoutCommission,
-					 _addTransactionDialogViewModel.PayoutPeriod,
-					 _addTransactionDialogViewModel.AssetType,
-					 _addTransactionDialogViewModel.Sector,
-					  _addTransactionDialogViewModel.Market);
+            Holding holding = new Holding(_addTransactionViewModel.Name,
+                                          _addTransactionViewModel.Ticker,
+                                          _addTransactionViewModel.Quantity,
+                                          DateOnly.FromDateTime(_addTransactionViewModel.Date),
+                                          _addTransactionDialogViewModel.PayoutYield,
+                                          _addTransactionDialogViewModel.PayoutTax,
+                                          _addTransactionDialogViewModel.PayoutCommission,
+                                          _addTransactionDialogViewModel.PayoutPeriod,
+                                          _addTransactionDialogViewModel.AssetType,
+                                          _addTransactionDialogViewModel.Sector,
+                                          _addTransactionDialogViewModel.Market);
 
-			holding.addTrade(trade);
-			_portfolio.AddToHoldings(holding);
+            holding.addTrade(trade);
+            _portfolio.AddToHoldings(holding);
 
-			base.Execute(parameter);
-		}
+            base.Execute(parameter);
+        }
 
-		/// <summary>
-		/// Prompts the user to input additional info for holdings that do not appear in the portfolio yet.
-		/// </summary>
-		/// <returns></returns>
-		private bool getAdditionalInfo()
-		{
-			AddTransactionDialog dialog = new AddTransactionDialog()
-			{
-				DataContext = _addTransactionDialogViewModel
-			};
+        /// <summary>
+        /// Prompts the user to input additional info for holdings that do not appear in the portfolio yet.
+        /// </summary>
+        /// <returns></returns>
+        private bool getAdditionalInfo()
+        {
+            AddTransactionDialog dialog = new AddTransactionDialog()
+                                          {
+                                              DataContext = _addTransactionDialogViewModel
+                                          };
 
-			if (dialog.ShowDialog() == false)
-			{
-				MessageBox.Show("Must fill details to add transaction.");
-				return false;
-			}
-			return true;
-		}
+            if (dialog.ShowDialog() == false)
+            {
+                MessageBox.Show("Must fill details to add transaction.");
+                return false;
+            }
 
-		/// <summary>
-		/// Handles the property changes in AddTransactionViewModel by checking if they affect the value of CanExecute.
-		/// </summary>
-		/// <param name="sender">The object that triggered the event. Not used and can be set to null.</param>
-		/// <param name="e">Event arguments containing information about the event. Not used and can be set to null.</param>
-		private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
-		{
-			if (e.PropertyName == nameof(AddTransactionViewModel.Name) ||
-				e.PropertyName == nameof(AddTransactionViewModel.Date) ||
-				e.PropertyName == nameof(AddTransactionViewModel.Quantity) ||
-				e.PropertyName == nameof(AddTransactionViewModel.Rate))
-			{
-				OnCanExecuteChanged();
-			}
-		}
+            return true;
+        }
 
-	}
+        /// <summary>
+        /// Handles the property changes in AddTransactionViewModel by checking if they affect the value of CanExecute.
+        /// </summary>
+        /// <param name="sender">The object that triggered the event. Not used and can be set to null.</param>
+        /// <param name="e">Event arguments containing information about the event. Not used and can be set to null.</param>
+        private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(AddTransactionViewModel.Name) ||
+                e.PropertyName == nameof(AddTransactionViewModel.Date) ||
+                e.PropertyName == nameof(AddTransactionViewModel.Quantity) ||
+                e.PropertyName == nameof(AddTransactionViewModel.Rate))
+            {
+                OnCanExecuteChanged();
+            }
+        }
+    }
 }
--- PortfolioTracker/Commands/NavigateCommand.cs
+++ PortfolioTracker/Commands/NavigateCommand.cs
@@ -4,21 +4,21 @@ using System;
 
 namespace PortfolioTracker.Commands
 {
-	public class NavigateCommand<TViewModel> : CommandsBase
-		where TViewModel : ViewModelBase
-	{
-		private readonly NavigationStore _navigationStore;
-		private readonly Func<TViewModel> _createViewModel;
+    public class NavigateCommand<TViewModel> : CommandsBase
+        where TViewModel : ViewModelBase
+    {
+        private readonly NavigationStore _navigationStore;
+        private readonly Func<TViewModel> _createViewModel;
 
-		public NavigateCommand(NavigationStore navigationStore, Func<TViewModel> createViewModel)
-		{
-			this._navigationStore = navigationStore;
-			this._createViewModel = createViewModel;
-		}
+        public NavigateCommand(NavigationStore navigationStore, Func<TViewModel> createViewModel)
+        {
+            this._navigationStore = navigationStore;
+            this._createViewModel = createViewModel;
+        }
 
-		public override void Execute(object? parameter)
-		{
-			_navigationStore.CurrentViewModel = _createViewModel();
-		}
-	}
+        public override void Execute(object? parameter)
+        {
+            _navigationStore.CurrentViewModel = _createViewModel();
+        }
+    }
 }
